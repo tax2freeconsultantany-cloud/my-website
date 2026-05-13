cc6bb46bf42d1efc1038e439464e5097dc4997eb
tax2freeconsultantany-cloud <tax2freeconsultantany@gmail.com>
2026-05-13 23:37:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 //  UNIVERSAL PWA SW (GitHub + Netlify)
 // =============================
 
-const CACHE_NAME = "my-website-pwa-v17";
+const CACHE_NAME = "my-website-pwa-v18";
 
 // 🔥 AUTO detect base path
 const BASE = self.location.hostname.includes("github.io")
